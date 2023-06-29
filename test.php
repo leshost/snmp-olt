a674a6bd284e10842c05bc8a8c82ef98f9a19471
Andrii Leskiv <leshost@gmail.com>
2023-06-29 15:23:30 +0300

    Update: formatting

--- test.php
+++ test.php
@@ -1,242 +1,178 @@
+<?php
+    # Config
+    $ip='10.10.0.4'; // IP addres
+    $ro='public'; // Community
+?>
 <html>
+
 <head>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
+
+    <script src="assets/js/script.js"></script>
 </head>
 
 <body>
-<form>
-  <input type="button" value="Върни се назад!" onclick="history.back()">
-</form>
-<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Потърси по име на ону-то">
-
-<script>
-function myFunction() {
-  // Declare variables
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-</script>
-
-<script>
-function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("myTable");
-  switching = true;
-  //Set the sorting direction to ascending:
-  dir = "asc"; 
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /*check if the two rows should switch place,
-      based on the direction, asc or desc:*/
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      //Each time a switch is done, increase this count by 1:
-      switchcount ++;      
-    } else {
-      /*If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again.*/
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
-</script>
-
+    <form>
+        <input type="button" value="Върни се назад!" onclick="history.back()">
+    </form>
+    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search by name">
 
 <?php
 
 $time1= date("Y-m-d H:i:s");
-$ip='10.10.0.4';
-$ro='public';
 $session = new SNMP(SNMP::VERSION_2C, $ip, $ro);
-$ifDescr = $session->walk(".1.3.6.1.2.1.2.2.1.2", TRUE);
 
-$ifAlias = $session->walk("IF-MIB::ifAlias", TRUE);
+$ifDescr        = $session->walk(".1.3.6.1.2.1.2.2.1.2", TRUE);
+$ifAlias        = $session->walk("IF-MIB::ifAlias", TRUE);
+$ifSpeed        = $session->walk(".1.3.6.1.2.1.2.2.1.5", TRUE);
+$ifAdminStatus  = $session->walk(".1.3.6.1.2.1.2.2.1.7", TRUE);
+$ifOperStatus   = $session->walk(".1.3.6.1.2.1.2.2.1.8", TRUE);
+$ifInErrors     = $session->walk(".1.3.6.1.2.1.2.2.1.14", TRUE);
+$ifOutErrors    = $session->walk(".1.3.6.1.2.1.2.2.1.20", TRUE);
+$ONUMAC         = $session->walk("1.3.6.1.4.1.3320.101.10.1.1.3", TRUE);
+$ONURxLevel     = $session->walk("1.3.6.1.4.1.3320.101.10.5.1.5", TRUE);
+$ONUTemp        = $session->walk("1.3.6.1.4.1.3320.101.10.5.1.2", TRUE);
+$ONUDist        = $session->walk("1.3.6.1.4.1.3320.101.10.1.1.27", TRUE);
+$ONUVendor      = $session->walk("1.3.6.1.4.1.3320.101.10.1.1.1", TRUE);
+$ONUModel       = $session->walk("1.3.6.1.4.1.3320.101.10.1.1.2", TRUE);
+$Timeticks      = $session->walk("iso.3.6.1.2.1.2.2.1.9", TRUE);
+$sysuptime      = $session->walk("SNMPv2-MIB::sysUpTime.0", TRUE);
 
-$ifSpeed = $session->walk(".1.3.6.1.2.1.2.2.1.5", TRUE);
-
-$ifAdminStatus = $session->walk(".1.3.6.1.2.1.2.2.1.7", TRUE);
-
-$ifOperStatus = $session->walk(".1.3.6.1.2.1.2.2.1.8", TRUE);
-
-$ifInErrors = $session->walk(".1.3.6.1.2.1.2.2.1.14", TRUE);
-
-$ifOutErrors = $session->walk(".1.3.6.1.2.1.2.2.1.20", TRUE);
-
-$ONUMAC = $session->walk("1.3.6.1.4.1.3320.101.10.1.1.3", TRUE);
-
-$ONURxLevel = $session->walk("1.3.6.1.4.1.3320.101.10.5.1.5", TRUE);
-
-$ONUTemp = $session->walk("1.3.6.1.4.1.3320.101.10.5.1.2", TRUE);
-
-$ONUDist = $session->walk("1.3.6.1.4.1.3320.101.10.1.1.27", TRUE);
-
-$ONUVendor = $session->walk("1.3.6.1.4.1.3320.101.10.1.1.1", TRUE);
-
-$ONUModel = $session->walk("1.3.6.1.4.1.3320.101.10.1.1.2", TRUE);
-
-$Timeticks = $session->walk("iso.3.6.1.2.1.2.2.1.9", TRUE);
-
-$sysuptime = $session->walk("SNMPv2-MIB::sysUpTime.0", TRUE);
 //$sysuptime[1] = preg_replace("Timeticks:","",$sysuptime[0]);
+
 print_r($sysuptime);
 echo "<br>";
 
 //echo '<td>'.$sysuptime.'</td>';
 
 		asort($ifDescr);
-        foreach ($ifDescr as $key => $value) {
-        $iface[$key]['IfId']=$key;
-        $value=explode(' ', $value);
-        $value=end($value);
-        $value=trim($value);
-        $value = str_replace("\"", "", $value);
-        $iface[$key]['IfDescr']=$value;
+        foreach ($ifDescr as $key => $value) 
+        {
+            $iface[$key]['IfId']=$key;
+            $value=explode(' ', $value);
+            $value=end($value);
+            $value=trim($value);
+            $value = str_replace("\"", "", $value);
+            $iface[$key]['IfDescr']=$value;
         }
-        foreach ($ifAlias as $key => $value) {
-        $iface[$key]['IfId']=$key;
-        $value=explode(' ', $value);
-        $value=end($value);
-        $value=trim($value);
-        $value = str_replace("\"", "", $value);
-        $iface[$key]['ifAlias']=$value;
+        
+        foreach ($ifAlias as $key => $value) 
+        {
+            $iface[$key]['IfId']=$key;
+            $value=explode(' ', $value);
+            $value=end($value);
+            $value=trim($value);
+            $value = str_replace("\"", "", $value);
+            $iface[$key]['ifAlias']=$value;
         }
-	foreach ($Timeticks as $key => $value) {
-//        $iface[$key]['Timeticks']=$value;
-        $value=explode('  ', $value);
-        $value=end($value);
-//	$value = str_replace("Timeticks:","",$value[0]);
-//	$value = str_replace("", "", $value);
+	
+        foreach ($Timeticks as $key => $value) 
+        {
+            //        $iface[$key]['Timeticks']=$value;
+            $value=explode('  ', $value);
+            $value=end($value);
+            //	$value = str_replace("Timeticks:","",$value[0]);
+            //	$value = str_replace("", "", $value);
 
-	$value = explode(')', $value);
-	$value = trim(end($value));
+            $value = explode(')', $value);
+            $value = trim(end($value));
 
-	$iface[$key]['Timeticks']=$value;
-        }
-	foreach ($ifSpeed as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $iface[$key]['IfSpeed']=$value;
-        }
-        foreach ($ifAdminStatus as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $iface[$key]['IfAdminStatus']=$value;
-        }
-        foreach ($ifOperStatus as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $iface[$key]['IfOperStatus']=$value;
-        }
-        foreach ($ifInErrors as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $iface[$key]['IfInErrors']=$value;
-        }
-        foreach ($ifOutErrors as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $iface[$key]['IfOutErrors']=$value;
-        }
-        foreach ($ONUMAC as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $value = str_replace (" ", ":", $value);
-        $iface[$key]['ONUMAC']=$value;
-        }
-        foreach ($ONURxLevel as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $iface[$key]['ONURxLevel']=$value;
-        }
-        foreach ($ONUTemp as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $value = $value/256;
-        $value = round($value, 2);
-        $iface[$key]['ONUTemp']=$value;
-        }
-        foreach ($ONUDist as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $iface[$key]['ONUDist']=$value;
-        }
-        foreach ($ONUVendor as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $value = str_replace("\"", "", $value);
-        $iface[$key]['ONUVendor']=$value;
-        }
-        foreach ($ONUModel as $key => $value) {
-        $value=explode(':', $value);
-        $value=end($value);
-        $value=trim($value);
-        $value = str_replace("\"", "", $value);
-        $iface[$key]['ONUModel']=$value;
+            $iface[$key]['Timeticks']=$value;
         }
 
+	    foreach ($ifSpeed as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $iface[$key]['IfSpeed']=$value;
+        }
 
+        foreach ($ifAdminStatus as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $iface[$key]['IfAdminStatus']=$value;
+        }
 
+        foreach ($ifOperStatus as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $iface[$key]['IfOperStatus']=$value;
+        }
 
+        foreach ($ifInErrors as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $iface[$key]['IfInErrors']=$value;
+        }
 
+        foreach ($ifOutErrors as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $iface[$key]['IfOutErrors']=$value;
+        }
+
+        foreach ($ONUMAC as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $value = str_replace (" ", ":", $value);
+            $iface[$key]['ONUMAC']=$value;
+        }
+
+        foreach ($ONURxLevel as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $iface[$key]['ONURxLevel']=$value;
+        }
+
+        foreach ($ONUTemp as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $value = $value/256;
+            $value = round($value, 2);
+            $iface[$key]['ONUTemp']=$value;
+        }
+
+        foreach ($ONUDist as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $iface[$key]['ONUDist']=$value;
+        }
+
+        foreach ($ONUVendor as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $value = str_replace("\"", "", $value);
+            $iface[$key]['ONUVendor']=$value;
+        }
+
+        foreach ($ONUModel as $key => $value) 
+        {
+            $value=explode(':', $value);
+            $value=end($value);
+            $value=trim($value);
+            $value = str_replace("\"", "", $value);
+            $iface[$key]['ONUModel']=$value;
+        }
 
 	echo "Генерирано на: $time1.<h2></h2>";
 	echo "<table id='myTable'>";
@@ -249,63 +185,88 @@ echo "<br>";
         echo "<th onclick='sortTable(6)'>Температура</th>";
         echo "<th onclick='sortTable(7)'>Разстояние в метри</th>";
 	echo "</tr>";
-	foreach ($iface as $key){
-	$date=date("Y-m-d H:i:s");
-//        $IfId=$equipment_id.'_'.$key['IfId'];
-//	$Timeticks=$equipment_id.'  '.$key['IfId'];
+	
+    foreach ($iface as $key)
+    {
+        $date=date("Y-m-d H:i:s");
+        // $IfId=$equipment_id.'_'.$key['IfId'];
+        // $Timeticks=$equipment_id.'  '.$key['IfId'];
         $IfDescr=$key['IfDescr'];
         $ifAlias=$key['ifAlias'];
         $IfSpeed=$key['IfSpeed'];
-	$Timeticks=$key['Timeticks'];
+	    $Timeticks=$key['Timeticks'];
         $IfAdminStatus=$key['IfAdminStatus'];
         $IfOperStatus=$key['IfOperStatus'];
         $IfInErrors=$key['IfInErrors'];
         $IfOutErrors=$key['IfOutErrors'];
-        if(isset( $key['ONUMAC'])){
-        $ONUMAC=$key['ONUMAC'];}else{$ONUMAC=NULL;}
-        if(isset( $key['ONURxLevel'])){
-        $ONURxLevel=$key['ONURxLevel']/10;}else{$ONURxLevel=NULL;}
-        if(isset( $key['ONUTemp'])){
-        $ONUTemp=$key['ONUTemp'];}else{$ONUTemp=NULL;}
-        if(isset( $key['ONUDist'])){
-        $ONUDist=$key['ONUDist'];}else{$ONUDist=NULL;}
-//      if(isset( $key['ONUVendor'])){
-//      $ONUVendor=$key['ONUVendor'];}else{$ONUVendor=NULL;}
-//      if(isset( $key['ONUModel'])){
-//      $ONUModel=$key['ONUModel'];}else{$ONUModel=NULL;}
-//      $ONUVendorModel=$ONUVendor.'/'.$ONUModel;
-//	echo '<td>IfId: '.$IfId.'</td>';
-//	echo '<br>';
+        
+        $ONUMAC=NULL;
+        if(isset( $key['ONUMAC'])) 
+        {
+            $ONUMAC=$key['ONUMAC'];
+        }
+
+        $ONURxLevel=NULL;
+        if(isset( $key['ONURxLevel'])) 
+        {
+            $ONURxLevel=$key['ONURxLevel']/10;
+        }
+
+        $ONUTemp=NULL;
+        if(isset( $key['ONUTemp']))
+        {
+            $ONUTemp=$key['ONUTemp'];
+        }
+
+        $ONUDist=NULL;
+        if(isset( $key['ONUDist']))
+        {
+            $ONUDist=$key['ONUDist'];
+        }
+
+        // $ONUVendor=NULL;if(isset( $key['ONUVendor'])){
+        // $ONUVendor=$key['ONUVendor'];}else{}
+        // if(isset( $key['ONUModel'])){
+        // $ONUModel=$key['ONUModel'];}else{$ONUModel=NULL;}
+        // $ONUVendorModel=$ONUVendor.'/'.$ONUModel;
+        // echo '<td>IfId: '.$IfId.'</td>';
+        // echo '<br>';
+
         echo '<td>'.$IfDescr.'</td>';
         echo '<td>'.$ifAlias.'</td>';
-//        echo '<td>IfSpeed: '.$IfSpeed.'</td>';
-//        echo '<td>IfAdminStatus: '.$IfAdminStatus.'</td>';
-		$statusColor	= stripos( $IfOperStatus, 'up' ) === false ? 'red' : 'green';
+        
+        // echo '<td>IfSpeed: '.$IfSpeed.'</td>';
+        // echo '<td>IfAdminStatus: '.$IfAdminStatus.'</td>';
+		
+        $statusColor	= stripos( $IfOperStatus, 'up' ) === false ? 'red' : 'green';
         echo '<td style="background-color:'.$statusColor.'">&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-//        echo '<td>IfInErrors: '.$IfInErrors.'</td>';
-//        echo '<td>IfOutErrors: '.$IfOutErrors.'</td>';
-//	echo '</br>';
-	echo '<td>'.$Timeticks.'</td>';
+        
+        // echo '<td>IfInErrors: '.$IfInErrors.'</td>';
+        // echo '<td>IfOutErrors: '.$IfOutErrors.'</td>';
+        // echo '</br>';
+	    
+        echo '<td>'.$Timeticks.'</td>';
+
         $epon=stripos($IfDescr, 'pon');
         $eponslash=stripos($IfDescr, '/');
         $eponcolon=stripos($IfDescr, ':');
-        if($epon !== false and $eponslash!== false and $eponcolon !== false){
-        echo '<td>MAC: '.$ONUMAC.'</td>';
-        echo '<td>Сигнал: '.$ONURxLevel.'</td>';
-        echo '<td>Температура: '.$ONUTemp.'</td>';
-        echo '<td>Растояние: '.$ONUDist.'</td>';}
-//        echo '<td>ONUVendor: '.$ONUVendor.'</td>';
-//        echo '<td>ONUModel: '.$ONUModel.'</td>'; }
-//	echo '</br>';
-	echo '</tr>';
-}
+        
+        if($epon !== false and $eponslash!== false and $eponcolon !== false)
+        {
+            echo '<td>MAC: '.$ONUMAC.'</td>';
+            echo '<td>Сигнал: '.$ONURxLevel.'</td>';
+            echo '<td>Температура: '.$ONUTemp.'</td>';
+            echo '<td>Растояние: '.$ONUDist.'</td>';
+        }
+
+        // echo '<td>ONUVendor: '.$ONUVendor.'</td>';
+        // echo '<td>ONUModel: '.$ONUModel.'</td>'; }
+        // echo '</br>';
+        
+        echo '</tr>';
+    }
 
 	echo '</table>';
-
-
-
 ?>
-
 </body>
 </html>
-
